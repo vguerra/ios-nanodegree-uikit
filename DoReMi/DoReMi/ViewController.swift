88fc279feac41544e3bda3b306594ee02dd71888
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Air.local>
2015-02-11 18:35:01 -0800

    adding DoReMi dictionary and implementations for dataSource methods

--- DoReMi/DoReMi/ViewController.swift
+++ DoReMi/DoReMi/ViewController.swift
@@ -18,17 +18,30 @@ class ViewController: UIViewController, UITableViewDataSource {
     // Choose some data to show in your table
     
     let model = [
-        // TODO: Fill this array with data
+        ["text" : "Do", "detail" : "A deer. A female deer."],
+        ["text" : "Re", "detail" : "A drop of golden sun."],
+        ["text" : "Mi", "detail" : "A name, I call myself."],
+        ["text" : "Fa", "detail" : "A long long way to run."],
+        ["text" : "So", "detail" : "A needle pulling thread."],
+        ["text" : "La", "detail" : "A note to follow So."],
+        ["text" : "Ti", "detail" : "A drink with jam and bread."]
     ]
     
     // Add the two essential table data source methods here
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //TODO: Implement method to return the correct number of rows.
+        return self.model.count;
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //TODO: Implement method to return cell with the correct reuseidentifier and populated with the correct data.
+        let cell =  tableView.dequeueReusableCellWithIdentifier(self.cellReuseIdentifier) as UITableViewCell
+        
+        let dictionary = self.model[indexPath.row]
+        
+        cell.textLabel?.text = dictionary["text"]
+        cell.detailTextLabel!.text = dictionary["detail"]
+        
+        return cell
     }
 
 }
