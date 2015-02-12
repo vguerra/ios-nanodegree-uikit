4422421bece27a025a19ae52f466ad7eeca6afd9
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Air.local>
2015-02-12 15:57:40 -0800

    adding code for the startOver button

--- TheTiger/TheTiger/ViewController.swift
+++ TheTiger/TheTiger/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: UIBarButtonItemStyle.Plain, target: self, action: "startOver")
     }
+    
+    func startOver() {
+        
+        if let navigationController = self.navigationController? {
+            navigationController.popToRootViewControllerAnimated(true)
+        }
+    }
+
 }
 
