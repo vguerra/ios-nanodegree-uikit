5df53eeca218933c96c4543c96ab9bedddec9ac7
Chrisna Aing <chrisna@chrisna.org>
2015-03-01 19:39:02 -0800

    Fix typos and spacing.

--- BondVillains/BondVillains/Villain.swift
+++ BondVillains/BondVillains/Villain.swift
@@ -51,19 +51,19 @@ extension Villain {
     
     static func localVillainData() -> [[String : String]] {
         return [
-            [Villain.NameKey : "Mr. Big", Villain.EvilSchemeKey : "Smuggle herion.",  Villain.ImageNameKey : "Big"],
-            [Villain.NameKey : "Ernest Blofeld", Villain.EvilSchemeKey : "Many, many, schemes.",  Villain.ImageNameKey : "Blofeld"],
-            [Villain.NameKey : "Sir Hugo Drax", Villain.EvilSchemeKey : "Nerve gass Earth, from the Moon.",  Villain.ImageNameKey : "Drax"],
-            [Villain.NameKey : "Jaws", Villain.EvilSchemeKey : "Kill Bond with huge metal teeth.",  Villain.ImageNameKey : "Jaws"],
-            [Villain.NameKey : "Rosa Klebb", Villain.EvilSchemeKey : "Humiliate MI6",  Villain.ImageNameKey : "Klebb"],
+            [Villain.NameKey : "Mr. Big", Villain.EvilSchemeKey : "Smuggle heroin.", Villain.ImageNameKey : "Big"],
+            [Villain.NameKey : "Ernest Blofeld", Villain.EvilSchemeKey : "Many, many schemes.", Villain.ImageNameKey : "Blofeld"],
+            [Villain.NameKey : "Sir Hugo Drax", Villain.EvilSchemeKey : "Nerve gas Earth from the Moon.", Villain.ImageNameKey : "Drax"],
+            [Villain.NameKey : "Jaws", Villain.EvilSchemeKey : "Kill Bond with huge metal teeth.", Villain.ImageNameKey : "Jaws"],
+            [Villain.NameKey : "Rosa Klebb", Villain.EvilSchemeKey : "Humiliate MI6.", Villain.ImageNameKey : "Klebb"],
             [Villain.NameKey : "Emilio Largo", Villain.EvilSchemeKey : "Steal nuclear weapons", Villain.ImageNameKey : "EmilioLargo"],
-            [Villain.NameKey : "Le Chiffre", Villain.EvilSchemeKey : "Beat bond at poker.",  Villain.ImageNameKey : "Lechiffre"],
-            [Villain.NameKey : "Odd Job", Villain.EvilSchemeKey : "Kill Bond with razor hat.",  Villain.ImageNameKey : "OddJob"],
-            [Villain.NameKey : "Francisco Scaramanga", Villain.EvilSchemeKey : "Kill Bond after assembling a golden gun.",  Villain.ImageNameKey : "Scaramanga"],
-            [Villain.NameKey : "Raoul Silva", Villain.EvilSchemeKey : "Kill M.",  Villain.ImageNameKey : "Silva"],
-            [Villain.NameKey : "Alec Trevelyan", Villain.EvilSchemeKey : "Nuke London, after killing Bond.",  Villain.ImageNameKey : "Trevelyan"],
-            [Villain.NameKey : "Auric Goldfinger", Villain.EvilSchemeKey : "Nuke Fort Knox.",  Villain.ImageNameKey : "Goldfinger"],
-            [Villain.NameKey : "Max Zorin", Villain.EvilSchemeKey : "Destroy Silicon Valley with an earthquake and flood.",  Villain.ImageNameKey : "Zorin"]
+            [Villain.NameKey : "Le Chiffre", Villain.EvilSchemeKey : "Beat Bond at poker.", Villain.ImageNameKey : "Lechiffre"],
+            [Villain.NameKey : "Odd Job", Villain.EvilSchemeKey : "Kill Bond with a razor hat.", Villain.ImageNameKey : "OddJob"],
+            [Villain.NameKey : "Francisco Scaramanga", Villain.EvilSchemeKey : "Kill Bond after assembling a golden gun.", Villain.ImageNameKey : "Scaramanga"],
+            [Villain.NameKey : "Raoul Silva", Villain.EvilSchemeKey : "Kill M.", Villain.ImageNameKey : "Silva"],
+            [Villain.NameKey : "Alec Trevelyan", Villain.EvilSchemeKey : "Nuke London after killing Bond.", Villain.ImageNameKey : "Trevelyan"],
+            [Villain.NameKey : "Auric Goldfinger", Villain.EvilSchemeKey : "Nuke Fort Knox.", Villain.ImageNameKey : "Goldfinger"],
+            [Villain.NameKey : "Max Zorin", Villain.EvilSchemeKey : "Destroy Silicon Valley with an earthquake and flood.", Villain.ImageNameKey : "Zorin"]
         ]
     }
 }
